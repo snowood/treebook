540e92771c780f9b8a5ea0e6e2c52be6258beb56
Tom Wood <tom@rtown.ca>
2014-03-08 14:24:37 -0800

    Created a rail app, and learnt alot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = 'ea225fe833121f728a177386aa4d6de2d5e7bff703f9ec3d5e2f41dcb6985139cdb4784aaab5e360fb0f674072646841594c0d68a3fa8e0eafdd34092e7fecf4'
